40fe999c48c93e2948033fe32082704f16c9bcfb
josefragala <joseffn2001@gmail.com>
2026-09-01 07:05:05 -0300

    complete address CRUD with validation and exception handling

---
+++ src/main/java/com/fragala/techstore/exception/ApiError.java
@@ -0,0 +1,34 @@
+package com.fragala.techstore.exception;
+
+import java.util.Map;
+
+public class ApiError {
+
+    private int status;
+    private String message;
+    private Map<String, String> errors;
+
+    public ApiError(int status, String message){
+        this.status = status;
+        this.message = message;
+    }
+
+    public ApiError(int status, String message, Map<String, String> errors) {
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public Map<String, String> getErrors(){
+        return errors;
+    }
+    
+}
--- src/main/java/com/fragala/techstore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fragala/techstore/exception/GlobalExceptionHandler.java
@@ -1,7 +1,13 @@
 package com.fragala.techstore.exception;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,10 +15,42 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class) // "Whenever a ResourceNotFoundException reaches the controller layer, execute this method"
-    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException exception){
+    public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException exception){
+        
+        ApiError error =  new ApiError(
+            HttpStatus.NOT_FOUND.value(),
+            exception.getMessage()
+        );
+        
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
+                .body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleValidationException(
+        MethodArgumentNotValidException exception) {
+
+            List<FieldError> errors = exception.getBindingResult().getFieldErrors();
+
+            Map<String, String> validationErrors = new HashMap<>();
+
+            for (FieldError error : errors){
+                validationErrors.put(
+                    error.getField(),
+                    error.getDefaultMessage() 
+                );
+            }
+
+            ApiError error = new ApiError(
+                HttpStatus.BAD_REQUEST.value(),
+                    "Validation failed", validationErrors
+                );
+
+            return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
+        }
+    
     
 }
